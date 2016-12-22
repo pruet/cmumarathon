40c12ab953a0d2bf10a69f13c63468c974c6fc95
pruet <pruetboonma@gmail.com>
2016-12-22 14:33:05 +0700

    fix config file

--- login.php
+++ login.php
@@ -1,11 +1,12 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/config.php';
 
 $fb = new Facebook\Facebook([
-  'app_id' => '1032499896860006',
-  'app_secret' => 'f56567b7d5e4c336ffc59aa382241f3a',
-  'default_graph_version' => 'v2.5',
+  'app_id' => $app_id,
+  'app_secret' => $app_secret,
+  'default_graph_version' => $default_graph_version,
 ]);
 
 $helper = $fb->getRedirectLoginHelper();
