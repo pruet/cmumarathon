1b60a07d2654848264a6b2bea9e8e92e5343cc13
pruet <pruetboonma@gmail.com>
2016-12-26 16:08:02 +0700

    add fb code

--- post-update.php
+++ post-update.php
@@ -28,45 +28,42 @@ if(isset($bib) && isset($cp) && isset($time)) {
   $cursor = $coll->find($query);
   foreach($cursor as $doc) {
     $access_token = $doc["fbsession"];
-    echo $access_token;
+    $fb = new Facebook\Facebook([
+      'app_id' => $app_id,
+      'app_secret' => $app_secret,
+      'default_graph_version' => $default_graph_version,
+    ]);
+
+    $fb->setDefaultAccessToken($access_token);
+    try {
+      $response = $fb->get('/me');
+      $user = $response->getGraphUser();
+    } catch (FacebookResponseException $e) {
+      print_r($e);
+    }
+    if($user) {
+      try {
+        $photoCaption = 'My photo caption';
+        $post_data = array(
+          'message' => $photoCaption,
+          'url' => 'https://runnerapi.eng.cmu.ac.th/runnertracker/images/badge-c1.png'
+        );
+        $apiResponse = $fb->post('/me/photos', $post_data);
+        break;
+      } catch (FacebookApiException $e) {
+        $user = null;
+        print_r($e);
+      }
+    } else {
+      echo "no user";
+    }
   }
 }
-/*$access_token = 'EAAOrDZA2VPWYBAE04fAVZCyDFuE8ZCr8vZBd97M7YAe0ZA3PnIWPoLvTPHVZCzEJtWZCpZC4lXddAtAeNLwP8kBNpcIByv0zmeCplZCCZApohuaHYvxKrMZBI1fSZAikts5604V6cD8ZB9zPXsRdujknqcZCq9xV8klMuXGfHjXzKAWO3OkwZDZD';
-
-$fb = new Facebook\Facebook([
-  'app_id' => $app_id,
-  'app_secret' => $app_secret,
-  'default_graph_version' => $default_graph_version,
-]);
-
-$fb->setDefaultAccessToken($access_token);
-try {
-  $response = $fb->get('/me');
-  $user = $response->getGraphUser();
-} catch (FacebookResponseException $e) {
-  print_r($e);
-}
-if($user) {
-  try {
-    $photoCaption = 'My photo caption';
-    $post_data = array(
-      'message' => $photoCaption,
-      'url' => 'https://runnerapi.eng.cmu.ac.th/runnertracker/images/badge-c1.png'
-    );
-    $apiResponse = $fb->post('/me/photos', $post_data);
-  } catch (FacebookApiException $e) {
-    $user = null;
-    print_r($e);
-  }
-} else {
-  echo "no user";
-}*/
 ?>
 <table>
 <form method="post" action="/runnertracker/post-update.php" >
   <tr><td>BIB:</td><td><input type="text" name="bib" /></td></tr>
-  <tr><td>CP:</td><td><select><option value="1">1</option><option value="2">2</option><option value="3">3</option></select></td></tr>
-  <tr><td>CP:</td><td><input type="text" name="cp" /></td></tr>
+  <tr><td>CP:</td><td><select name="cp"><option value="1">1</option><option value="2">2</option><option value="3">3</option></select></td></tr>
   <tr><td>time:</td><td><input type="text" name="time" /></td></tr>
   <tr><td><input type="submit" /></td><td><td></tr>
 </form>
