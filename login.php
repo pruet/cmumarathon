3cddfda75e7f96f43e9d837a227ae87e90c4464c
pruet <pruetboonma@gmail.com>
2017-01-05 12:58:38 +0700

    add Javascript based login

--- login.php
+++ login.php
@@ -1,9 +1,39 @@
+<!DOCTYPE html>
 <html>
+<head>
+  <title>CMU Marathon Runner Tracker: Login</title>
+  <meta charset="UTF-8">
+  <meta name="description" content="Login page for CMU Marathon Runner Tracker Facebook App">
+  <meta name="keywords" content="CMU, Marathon, Tracker, Facebook">
+  <meta name="author" content="CMU Marathon Technical Team">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
 <body>
+
+<!--   facebook button code -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- end facebook button code -->
+
 Please read all the steps before start.<br />
 1. Please login with facebook using following link <br />
-<?php
+<div class="fb-login-button"
+     data-max-rows="1"
+     data-size="medium"
+     data-show-faces="true"
+     data-auto-logout-link="false"
+>
+</div>
 
+<?php
+/*
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/config.php';
 
@@ -23,7 +53,7 @@ $permissions = ['email', 'publish_actions', 'user_photos']; // optional
 $loginUrl = $helper->getLoginUrl('https://runnerapi.eng.cmu.ac.th/runnertracker/login-callback.php', $permissions);
 
 echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
-
+*/
 ?>
 <br />
 2. When Facebook ask for permission, please allow us to post on your wall.<br />
