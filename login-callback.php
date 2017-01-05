1b4acb19daf649ca347ecc5ec784a5ba0122edd5
pruet <pruetboonma@gmail.com>
2017-01-05 15:23:48 +0700

    update layout on all pages

--- login-callback.php
+++ login-callback.php
@@ -42,18 +42,57 @@ if (isset($accessToken)) {
   header('Location: https://runnerapi.eng.cmu.ac.th/runnertracker/login');
 }
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+  <title>CMU Marathon Runner Tracker: Login</title>
+  <meta charset="UTF-8">
+  <meta name="description" content="Login page for CMU Marathon Runner Tracker Facebook App">
+  <meta name="keywords" content="CMU, Marathon, Tracker, Facebook">
+  <meta name="author" content="CMU Marathon Technical Team">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="jquery.redirect.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <link href="starter-template.css" rel="stylesheet">
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
-Please provide us your BIB number and name.
-<table>
+      <div class="jumbotron" id="home">
+      <div class="container" >
+        <h1>Welcome to CMU Marathon Runner Tracker Facebook App</h1>
+        <p class="lead">
+          Please provide your bib number and name. The name will be shown on the badge post on your Facebook wall. 
+       </p>
 <form method="post" action="/runnertracker/register.php" >
   <input type="hidden" name="fbsession" value="<?php echo (string)$longLivedAccessToken ?>" />
-  <tr><td>BIB:</td><td><input type="text" name="bib" /></td></tr>
-  <tr><td>Name:</td><td><input type="text" name="runner" /></td></tr>
-  <tr><td><input type="submit" /></td><td><td></tr>
-</form>
+   <div class="row">
+        <div class="col-sm-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Bib number</h3>
+            </div>
+            <div class="panel-body">
+              <input type="text" name="bib" />
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Name</h3>
+            </div>
+            <div class="panel-body">
+              <input type="text" name="runner" />
+            </div>
+          </div>
+        </div>
+        </div>
+  <input type="submit" />
 </table>
 </body>
 </html
--- register.php
+++ register.php
@@ -1,3 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>CMU Marathon Runner Tracker: Login</title>
+  <meta charset="UTF-8">
+  <meta name="description" content="Login page for CMU Marathon Runner Tracker Facebook App">
+  <meta name="keywords" content="CMU, Marathon, Tracker, Facebook">
+  <meta name="author" content="CMU Marathon Technical Team">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="jquery.redirect.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <link href="starter-template.css" rel="stylesheet">
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+      <div class="jumbotron" id="home">
+      <div class="container" >
+        <h1>CMU Marathon Runner Tracker Facebook App</h1>
+        <p class="lead">
 <?php
   function clean($in)
   {
@@ -17,11 +41,7 @@
     $count = $db->runnertracker->count(array('bib' => $bib));
     if($count > 0) {
       ?>
-        <html>
-        <body>
         Error, duplicate bib number
-        </body>
-        </html>
         <?php
 
     } else {
@@ -33,22 +53,17 @@
       $ret = $coll->insert($document);
       if(isset($ret) && isset($ret["err"]) && $ret["err"] != NULL) {
         // nothing but lazy
-        ?>
-          <html>
-          <body>
+         ?>
           Error, try again. 
-          </body>
-          </html>
           <?php
       } else {
         ?>
-          <html>
-          <body>
           Thanks for registration, see you on the road!
-          </body>
-          </html>
         <?php
       }
     }
   }
 ?>
+</p></div></div>
+          </body>
+          </html>
