c2e71bb15ad2fb68bfe60efd0e31b150e1bb6cec
pruet <pruetboonma@gmail.com>
2017-01-20 16:23:35 +0700

    add webhook

---
+++ webhook.php
@@ -0,0 +1,106 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/config.php';
+
+function clean($in)
+{
+  return $in;
+  $t = trim($in);
+  $s = strip_tags($t);
+  $h = htmlspecialchars($s);
+  return $h;
+}
+
+function calculatePace($time, $cp)
+{
+  if($cp == 's') {
+    return 'N/A';
+  } else if($cp == 'f') {
+    $distance = 42.195;
+  } else {
+    $distance = (int)$cp * 10;
+  }
+  $str_time = preg_replace("/^([\d]{1,2})\:([\d]{2})$/", "00:$1:$2", $time);
+  sscanf($str_time, "%d:%d:%d", $hours, $minutes, $seconds);
+  $time_seconds = $hours * 3600 + $minutes * 60 + $seconds;
+  $pace_seconds = $time_seconds / $distance;
+  return ((int)($pace_seconds / 60)) . "'" . ($pace_seconds % 60) . '"';
+}
+
+if(!session_id()) {
+  session_start();
+}
+
+// Check request type
+$type = clean($_GET["type"]);
+if($type == 'json') {
+  $json = file_get_contents('php://input');
+  $js = json_decode($json);
+  $bib = clean($js->bib);
+  $cp = clean($js->cp);
+  $time = clean($js->time);
+  $pass = clean($js->tk);
+} else {
+  $bib = clean($_POST["bib"]);
+  $cp = clean($_POST["cp"]);
+  $time = clean($_POST["time"]);
+  $pass = clean($_POST["tk"]);
+}
+
+// Check parameters
+if(isset($pass) && ($pass == '7uZZs8RwpNnWjP5jHzsDTsA1CQGR') && isset($bib) && isset($cp) && isset($time)) {
+  $m = new MongoClient();
+  $db = $m->cmumarathon;
+  $collrt = $db->runnertracker;
+
+// check if user (bib) want to post at this location (cp)
+  $query = array('bib' => $bib, $cp => 'on');
+  if(($doc = $collrt->findOne($query)) != NULL) {
+    // check that we never post it before
+    if($db->postlog->count(array('bib' => $bib, $cp=> 'on')) == 0) {
+      $access_token = $doc["fbsession"];
+      $name = $doc["runner"];
+      $fb = new Facebook\Facebook([
+        'app_id' => $app_id,
+        'app_secret' => $app_secret,
+        'default_graph_version' => $default_graph_version,
+      ]);
+
+      $fb->setDefaultAccessToken($access_token);
+      try {
+        $response = $fb->get('/me');
+        $user = $response->getGraphUser();
+      } catch (FacebookResponseException $e) {
+        http_response_code(500);
+        print_r($e);
+        die();
+      }
+      if($user) {
+        $pace = calculatePace($time, $cp);
+        $image_base = 'https://runnerapi.eng.cmu.ac.th/runnertracker/genpng.php';
+        $image_query = 'cp=' . urlencode($cp) . '&name=' . urlencode($name) . '&time=' . urlencode($time) . '&pace=' . urlencode($pace);
+        $image = $image_base . '?' . $image_query;
+        try {
+          $post_data = array(
+            'url' => $image
+          );
+          $apiResponse = $fb->post('/me/photos', $post_data);
+          if(!$apiResponse->isError()) {
+            echo "Progress posted.<br />";
+            // save to log
+            $db->postlog->insert(array('bib' => $bib, $cp=>'on'));
+          }
+        } catch (FacebookApiException $e) {
+          $user = null;
+          http_response_code(500);
+          print_r($e);
+          die();
+        }
+      } else {
+        http_response_code(404);
+        header('HTTP/1.0 404 Not Found', true, 404);
+        die();
+      }
+    }
+  }
+}
