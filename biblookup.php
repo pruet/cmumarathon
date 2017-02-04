c86cc3aeaee1b2d5755e5a6405c72a37062277fd
pruet <pruetboonma@gmail.com>
2017-02-04 16:27:53 +0700

    add bib lookup function

---
+++ biblookup.php
@@ -0,0 +1,28 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/config.php';
+
+function clean($in)
+{
+  return $in;
+  $t = trim($in);
+  $s = strip_tags($t);
+  $h = htmlspecialchars($s);
+  return $h;
+}
+
+$bib = $_GET['bib'];
+$pass =$_GET['pass'];
+
+if(isset($pass) && ($pass == '7uZZs8RwpNnWjP5jHzsDTsA1CQGR') && isset($bib)) {
+  $m = new MongoClient();
+  $db = $m->cmumarathon;
+  $collinfo = $db->runnerinfo;
+  if(($doc = $db->runnerinfo->find(array('bib' => $bib))) != NULL) {
+    http_respones_code(200);
+    header('Content-type: text/javascript');
+    echo json_encode($doc, JSON_PRETTY_PRINT); 
+  }
+} else {
+   http_respones_code(404);
+}
