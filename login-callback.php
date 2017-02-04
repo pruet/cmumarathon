12239661f9d49e123f76d210d82b3419ca63f129
pruet <pruetboonma@gmail.com>
2017-02-04 18:22:34 +0700

    revise registration form

--- login-callback.php
+++ login-callback.php
@@ -12,21 +12,8 @@ $fb = new Facebook\Facebook([
   'default_graph_version' => $default_graph_version,
 ]);
 
-/*$_SESSION['FBRLH_state']=$_GET['state'];
-$helper = $fb->getRedirectLoginHelper();
-try {
-  $accessToken = $helper->getAccessToken();
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
-
-  echo 'Graph returned an error: ' . $e->getMessage();
-  exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
-  // When validation fails or other local issues
-  echo 'Facebook SDK returned an error: ' . $e->getMessage();
-  exit;
-}*/
-
 $accessToken = $_POST['accessToken'];
+$debug = $_GET['debug'];
 
 if (isset($accessToken)) {
   $oAuth2Client = $fb->getOAuth2Client();
@@ -37,7 +24,7 @@ if (isset($accessToken)) {
   $_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
   // Now you can redirect to another page and use the
   // access token from $_SESSION['facebook_access_token']
-} else {
+} else if($debug != 'true') {
   // sned back to login
   header('Location: https://cmumarathon.com/fblogin.html');
 }
@@ -63,19 +50,71 @@ if (isset($accessToken)) {
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   <script>
     function checkAll() {
-          document.getElementById("locationStart").checked = true;
-          document.getElementById("location10k").checked = true;
-          document.getElementById("location20k").checked = true;
-          document.getElementById("location30k").checked = true;
-          document.getElementById("locationFinish").checked = true;
+          document.getElementById("locationCS").checked = true;
+          document.getElementById("locationC1").checked = true;
+          document.getElementById("locationC2").checked = true;
+          document.getElementById("locationC3").checked = true;
+          document.getElementById("locationC4").checked = true;
+          document.getElementById("locationCF").checked = true;
     }
     function checkNone() {
-          document.getElementById("locationStart").checked = false;
-          document.getElementById("location10k").checked = false;
-          document.getElementById("location20k").checked = false;
-          document.getElementById("location30k").checked = false;
-          document.getElementById("locationFinish").checked = false;
+          document.getElementById("locationCS").checked = false;
+          document.getElementById("locationC1").checked = false;
+          document.getElementById("locationC2").checked = false;
+          document.getElementById("locationC3").checked = false;
+          document.getElementById("locationC4").checked = false;
+          document.getElementById("locationCF").checked = false;
     }
+    $(document).ready(function(){
+      $('#biblookup').click(function(){
+        var bib = $('#bib').val();
+        if(bib != undefined && bib != null && bib != "") {
+          var query = 'https://runnerapi.eng.cmu.ac.th/runnertracker/biblookup.php?bib='.concat(bib).concat('&pass=hohohohomerryxmas');
+          var jqxhr = $.getJSON(query, function(data) {
+          }).done(function(data) {
+            $('#bibsuccess').collapse('show'); 
+            var content = "<strong>First Name</strong> " + data.fname + "<br /><strong>Last name</strong> " + data.lname + "<br /><strong>Race type</strong> ";
+            if(data.type == "f") {
+              content = content + "Marathon";
+            } else if(data.type == "h") {
+              content = content + "Half marathon";
+              $('#lc3').hide();
+              $('#lc4').hide();
+            } else {
+              content = content + "Mini marathon";
+              $('#lc1').hide();
+              $('#lc3').hide();
+              $('#lc4').hide();
+            }
+            content = content + "<br /><strong>If the above information is correct, please proceed to the next step"
+            $('#bibsuccess').html(content); 
+            $('#panelName').collapse('show'); 
+            $('#panelMarker').collapse('show'); 
+            $('#panelSubmit').collapse('show'); 
+          }).fail(function(XMLHttpRequest, textStatus, errorThrown) {
+            $('#bibalert').collapse('show'); 
+            $('#bibalert').collapse('hide'); 
+            $('#bibsuccess').collapse('hide'); 
+            $('#panelName').collapse('hide'); 
+            $('#panelMarker').collapse('hide'); 
+            $('#panelSubmit').collapse('hide'); 
+          });
+        }
+      });
+      $('#runnername').change(function(){
+        var runnernanme = $('#runnername').val();
+        if(runnername != "") {
+           $(':input[type="submit"]').prop('disabled', false);
+        }
+      });
+      $('#runnerform').on('keyup keypress', function(e) {
+          var keyCode = e.keyCode || e.which;
+          if (keyCode === 13) { 
+            e.preventDefault();
+            return false;
+        }
+      });
+    });
   </script>
 </head>
 <body>
@@ -85,43 +124,54 @@ if (isset($accessToken)) {
     <p class="lead">
       Please provide your bib number, name and when you want to publish your progress. The name will be shown on the badge posted on your Facebook wall. If you have already provided the information, you are all set, please close this browser window and see you on the race day!.
     </p>
-<form method="post" action="/runnertracker/register.php">
+<form method="post" id="runnerform" action="/runnertracker/register.php">
   <input type="hidden" name="fbsession" value="<?php echo (string)$longLivedAccessToken ?>" />
    <div class="row">
-        <div class="col-sm-4">
-          <div class="panel panel-default">
+        <div class="col-sm-8">
+          <div class="panel panel-primary">
             <div class="panel-heading">
-              <h3 class="panel-title" class="control-label">Bib number</h3>
+              <h1 class="panel-title" class="control-label">1. Please provide your bib number</h1>
             </div>
             <div class="panel-body">
-              <input type="number" min="0" max="9999" maxlength="4" name="bib" class="form-control" required/> Only last four digit of your bib, e.g., if your bib is 18M1234, please input only 1234.
+              <p>
+              <input type="text" maxlength="4" name="bib" id="bib" class="form-control" required placeholder="Only last four digit of your bib, e.g., if your bib is 18M1234, please input only 1234." />
+              <input type="button" value="Look up" id="biblookup"/>
+              </p>
+              <p>
+              <div class="alert alert-danger collapse" role="alert" id="bibalert">Bib not found</div>
+              <div class="alert alert-success collapse" role="alert" id="bibsuccess">
+              </div>
+              </p>
             </div>
           </div>
         </div>
-        <div class="col-sm-4">
-          <div class="panel panel-default">
+     </div>
+     <div class="row">
+        <div class="col-sm-8">
+          <div class="panel panel-primary collapse" id="panelName">
             <div class="panel-heading">
-              <h3 class="panel-title" class="control-label">Name</h3>
+              <h1 class="panel-title" class="control-label">2. Please provide your name</h1>
             </div>
             <div class="panel-body">
-              <input type="text" name="runner" maxlength="10" class="form-control" required/> This name will be shown on the Facebook post, at most 10 characters.
+              <input type="text" id="runnername" name="runner" maxlength="10" class="form-control" placeholder="This name will be shown on the Facebook post, at most 10 characters." required />
             </div>
           </div>
         </div>
      </div>
      <div class="row">
        <div class="col-sm-8">
-         <div class="panel panel-default">
+         <div class="panel panel-primary collapse" id="panelMarker">
            <div class="panel-heading">
-          <h3 class="panel-title">When do you want to publish your progress on your facebook wall ?</h3>
-          </div>
+             <h1 class="panel-title">3. When do you want to publish your progress on your facebook wall ?</h1>
+           </div>
           <div class="panel-body">
             <div class="checkbox">
-              <label><input id="locationStart" type="checkbox" name="locationStart" checked />When I'm at the starting point</label><br />
-              <label><input id="location10k" type="checkbox" name="location10k" checked />When I pass 10k distance</label><br />
-              <label><input id="location20k" type="checkbox" name="location20k" checked />When I pass 20k distance</label><br />
-              <label><input id="location30k" type="checkbox" name="location30k" checked />When I pass 30k distance</label><br />
-              <label><input id="locationFinish" type="checkbox" name="locationFinish" checked />When I pass the finish line</label><br />
+              <label id="lcs"><input id="locationCS" type="checkbox" name="locationCS" checked />When I'm at the starting point</label><br />
+              <label id="lc1"><input id="locationC1" type="checkbox" name="locationC1" checked />When I pass Huay Tueng Tao</label><br />
+              <label id="lc2"><input id="locationC2" type="checkbox" name="locationC2" checked />When I pass Ton Payom market</label><br />
+              <label id="lc3"><input id="locationC3" type="checkbox" name="locationC3" checked />When I pass Ton Kwen Junction</label><br />
+              <label id="lc4"><input id="locationC4" type="checkbox" name="locationC4" checked />When I pass Royal Flora Ratchapruk Park</label><br />
+              <label id="lcf"><input id="locationCF" type="checkbox" name="locationCF" checked />When I pass the finish line</label><br />
               <button type="button" id="locationAll" class="btn btn-default" onClick="checkAll()">Check all</button>
               <button type="button" id="locationClear" class="btn btn-default" onClick="checkNone()">Clear all</button>
             </div>
@@ -129,7 +179,18 @@ if (isset($accessToken)) {
         </div>
        </div>
      </div>
-  <input type="submit" />
+     <div class="row">
+       <div class="col-sm-8">
+         <div class="panel panel-primary collapse" id="panelSubmit">
+           <div class="panel-heading">
+             <h1 class="panel-title">4. Please confirm all information before submitting</h1>
+           </div>
+          <div class="panel-body">
+          <input type="submit" id="submitbutton" disabled/>
+          </div>
+        </div>
+      </div>
+     </div>
 </table>
 </body>
 </html
