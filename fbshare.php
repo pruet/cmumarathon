31ec85f893766ce065ff23f7ff93a9f45a8b1102
pruet <pruetboonma@gmail.com>
2017-02-11 16:30:46 +0700

    update og: tag

--- fbshare.php
+++ fbshare.php
@@ -1,31 +1,23 @@
 <html>
-<body>
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/config.php';
-
-function clean($in)
-{
-  return $in;
-  $t = trim($in);
-  $s = strip_tags($t);
-  $h = htmlspecialchars($s);
-  return $h;
-}
-
 $bib = strval($_GET['bib']);
 
 // for dev
-header('Access-Control-Allow-Origin: *');  
+header('Access-Control-Allow-Origin: *');
 
 if(isset($bib)) {
   ?>
-    <head>
+     <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#">
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://runnerapi.eng.cmu.ac.th/runnertracker/fbshare.php?bib=<?php echo $bib ?>" />
   <meta property="og:title" content="CMU Marathon 2017 Unofficial Result" />
-  <meta property="og:image" content="https://runnerapi.eng.cmu.ac.th/runnertracker/genpngfb.php?bib="<?php echo $bib ?>" />
+  <meta property="og:description" content="CMU Marathon 2017 Unofficial Result" />
+  <meta property="og:site_name" content="CMU Marathon 2017 Unofficial Result" />
+  <meta property="og:image" content="https://runnerapi.eng.cmu.ac.th/runnertracker/genpngfb.php?bib=<?php echo $bib ?>" />
 </head>
 <body>
-    <img src="https://runnerapi.eng.cmu.ac.th/runnertracker/genpngfb.php?bib=<?php echo $bib ?>" alt="CMU Marathon 2017 Unofficial Result" width=900 height=1200 title="test" />
+CMU Marathon 2017 Unofficial Result<br />
+    <img src="https://runnerapi.eng.cmu.ac.th/runnertracker/genpngfb.php?bib=<?php echo $bib ?>" alt="CMU Marathon 2017 Unofficial Result" title="CMU Marathon 2017 Unofficial Result" />
   <?php
 }
 
