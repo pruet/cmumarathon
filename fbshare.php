3f07cf171b5dcebdf404fdbbe09029d805d7070d
pruet <pruetboonma@gmail.com>
2017-02-11 16:01:46 +0700

    misisng dquote

--- fbshare.php
+++ fbshare.php
@@ -22,7 +22,7 @@ if(isset($bib)) {
   ?>
     <head>
   <meta property="og:title" content="CMU Marathon 2017 Unofficial Result" />
-  <meta property="og:image" content="https://runnerapi.eng.cmu.ac.th/runnertracker/genpngfb.php?bib="<?php echo $bib ?> />
+  <meta property="og:image" content="https://runnerapi.eng.cmu.ac.th/runnertracker/genpngfb.php?bib="<?php echo $bib ?>" />
 </head>
 <body>
     <img src="https://runnerapi.eng.cmu.ac.th/runnertracker/genpngfb.php?bib=<?php echo $bib ?>" alt="CMU Marathon 2017 Unofficial Result" width=900 height=1200 title="test" />
