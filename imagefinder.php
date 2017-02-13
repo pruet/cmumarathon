c0e2a26836f9db59d128aff37f4de0f37e1835ab
pruet <pruetboonma@gmail.com>
2017-02-13 16:32:46 +0700

    image finder api

---
+++ imagefinder.php
@@ -0,0 +1,30 @@
+<?php
+require_once __DIR__ . '/config.php';
+
+$bib = strval($_GET['bib']);
+$pass =$_GET['pass'];
+
+// for dev
+header('Access-Control-Allow-Origin: *');  
+
+if(isset($pass) && ($pass == $imagefinder_pass) && isset($bib)) {
+  $m = new MongoClient();
+  $db = $m->cmumarathon;
+  if(($doc = $db->runnerimage->find(array('bib' => $bib))) != NULL) {
+    if($doc->count() == 0) {
+      // no image for this bib
+      http_response_code(404);
+      return;
+    }
+    http_response_code(200);
+    header('Content-type: text/javascript, charset=utf-8');
+    echo json_encode(iterator_to_array($adoc), JSON_PRETTY_PRINT); 
+    return;
+  } else {
+    // no bib
+    http_response_code(401);
+  }
+} else {
+  // incorrect pass
+   http_response_code(405);
+}
