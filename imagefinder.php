f08edbb7c6016ef4c94882bc0687729047845068
pruet <pruetboonma@gmail.com>
2017-02-15 23:14:50 +0700

    fix typo

--- imagefinder.php
+++ imagefinder.php
@@ -12,7 +12,7 @@ if(isset($pass) && ($pass == $imagefinder_pass) && isset($bib)) {
   $db = $m->cmumarathon;
   if(($doc = $db->runnerimage->find(array('bib' => $bib))) != NULL) {
     if($doc->count() == 0) {
-      // no image for this bib
+      // no image for this bibib
       http_response_code(404);
       return;
     }
@@ -25,6 +25,26 @@ if(isset($pass) && ($pass == $imagefinder_pass) && isset($bib)) {
     http_response_code(401);
   }
 } else {
+  $pass = $_POST['pass'];
+  if(isset($pass) && $pass == $imagefinder_pass) {
+    $action = $_POST['action'];
+    $url = $_POST['url'];
+    $bib = $_POST['bib'];
+    $oldbib = $_POST['oldbib'];
+    if($action == "delete") {
+      $db->runnerimage->remove(array('url' => $url, 'bib' => $bib));
+    } else {
+      $bibs = explode(',', $bib);
+      $doc = $db->runnerimage->findOne(array('url' => $url, 'bib' => $oldbib));
+      foreach($bibs as $bib) {
+        $bib = trim($bib);
+        $doc['bib'] = $bib;
+        $db->runnerimage->insert($doc);
+      }
+      $db->runnerimage->remove(array('url' => $url, 'bib' => $oldbib));
+    }
+  } else {
   // incorrect pass
    http_response_code(405);
+  }
 }
