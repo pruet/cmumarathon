c1e6fda50d786edfc7eeca8c86a0410eb8c94d78
pruet <pruetboonma@gmail.com>
2017-02-17 11:24:18 +0700

    add bootstrap

--- importimage.php
+++ importimage.php
@@ -53,6 +53,13 @@ if(isset($_POST['submit'])) {
 }
 ?>
 <html>
+  <head>
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <link href="starter-template.css" rel="stylesheet">
+  </head>
   <body>
     <div class="jumbotron" id="home">
       <div class="container" >
